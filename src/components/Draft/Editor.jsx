617b4af4ebda4822a98be0b47be61334bc83f2c4
owentashlin <owentashlin@gmail.com>
2022-12-02 19:31:55 -0800

    text effect buttons working

--- src/components/Draft/Editor.jsx
+++ src/components/Draft/Editor.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 import './Editor.css'
 
 class PageContainer extends React.Component {
@@ -16,12 +16,47 @@ class PageContainer extends React.Component {
     })
   }
 
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command)
+    if (newState) {
+      this.onChange(newState)
+      return 'handled'
+    } 
+    return 'not-handled'
+  }
+
+  onUnderlineClick = () => {
+		this.onChange(
+			RichUtils.toggleInlineStyle(this.state.editorState, "UNDERLINE")
+		);
+	};
+
+	onBoldClick = () => {
+		this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, "BOLD"));
+	};
+
+	onItalicClick = () => {
+		this.onChange(
+			RichUtils.toggleInlineStyle(this.state.editorState, "ITALIC")
+		);
+	};
+
   render() {
     return (
     <div className='editorContainer'> 
+        <button onClick={this.onUnderlineClick}><u>U</u></button>
+        &nbsp;
+				<button onClick={this.onBoldClick}>
+					<b>B</b>
+				</button>
+        &nbsp;
+				<button onClick={this.onItalicClick}>
+					<em>I</em>
+				</button>
       <div className='editors'>
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+        <Editor editorState={this.state.editorState} handleKeyCommand={this.handleKeyCommand} onChange={this.onChange} />
       </div>
+      <button className='save-button'>save</button>
     </div> 
     )
   }
