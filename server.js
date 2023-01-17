8ca7664ec3b436f1b78df9099793f5749ae33a7f
owentashlin <owentashlin@gmail.com>
2023-01-16 16:46:31 -0800

    working on editor

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
 
 require('dotenv').config();
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 // middleware that adds the user object from a JWT to req.user
@@ -31,8 +31,8 @@ app.get('/*', function(req, res) {
 
 module.exports = app
 
-// const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
-// app.listen(port, function() {
-//   console.log(`Express app running on port ${port}`);
-// });
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`);
+});
--- src/components/Draft/Editor.jsx
+++ src/components/Draft/Editor.jsx
@@ -1,11 +1,110 @@
-// import {setState, useState} from 'react'
-// import {Editor, EditorState, RichUtils, convertToRaw, convertFromRaw} from 'draft-js';
-// import debounce from 'lodash.debounce';
-// import SaveButton from './Save';
-// import './Draft.css'
-// import { set } from 'lodash';
-// import { useEffect } from 'react';
-// import axios from 'axios';
+import {setState, useState} from 'react'
+import {Editor, EditorState, RichUtils, convertToRaw, convertFromRaw} from 'draft-js';
+//import debounce from 'lodash.debounce';
+//import SaveButton from './Save';
+import './Draft.css'
+import { useEffect } from 'react';
+//import { set } from 'lodash';
+//import { useEffect } from 'react';
+//import axios from 'axios';
+
+function WritingApp() {
+    const [editorState, setEditorState] = useState(
+      () => EditorState.createEmpty())
+
+    const editor = useRef(null)
+
+    function focusEditor() {
+      editor.current.focus()
+    }
+
+    useEffect(() => {
+      focusEditor()
+    }, [])
+
+    const StyleButton = (props) => {
+      let onClickButton = (event) => {
+        event.preventDefault()
+        props.onToggle(props.style)
+      }
+      return <button onMouseDown = {onClickButton}></button>
+    }
+
+    const Block_Types = [
+      {label: 'H1', style: 'header-one'},
+      {label: 'H2', style: 'header-two'},
+      {label: 'H3', style: 'header-three'},
+      {label: 'H4', style: 'header-four'},
+      {label: 'H5', style: 'header-five'},
+      {label: 'H6', style: 'header-six'},
+      {label: 'Blockquote', style: 'blockquote'},
+      {label: 'UL', style: 'unordered-list-item'},
+      {label: 'OL', style: 'ordered-list-item'},
+      {label: 'Code Block', style: 'code-block'}
+    ]
+
+    const BlockStyleControls = (props) => {
+      return (
+        <div>
+          {Block_Types.map((type) =>(
+            <StyleButton
+              key={type.label}
+              label={type.label}
+              onToggle={props.onToggle}
+              style={type.style}
+              />
+          ))}
+        </div>
+      )
+    }
+
+    const Inline_Styles = [
+      {label: 'Bold', style:'BOLD'},
+      {label: 'Italic', style:'ITALIC'},
+      {label: 'Underline', style:'UNDERLINE'},
+      {label: 'Monospace', style:'CODE'}
+    ]
+
+    const InlineStyleControls = (props) => {
+      return (
+        <div>
+          {Inline_Styles.map((type) =>(
+            <StyleButton
+              key={type.label}
+              label={type.label}
+              onToggle={props.onToggle}
+              style={type.style}
+              />
+          ))}
+        </div>
+      )
+    }
+
+    const onInlineClick = (event) => {
+      let nextState = RichUtils.toggleInlineStyle(editorState, event)
+      setEditorState(nextState)
+    }
+
+    return (
+      <div className='editorContainer' onClick={focusEditor}>
+        <div className='editors'>
+          <BlockStyleControls onToggle={onBlockBlick}/>
+          <InlineStyleControls onToggle={onInlineClick}/>
+        </div>
+        <div className='editors'>
+          <Editor
+            ref={editor}
+            editorState={editorState}
+            onChange={(editorState) => setEditorState(editorState)}
+            />
+        </div>
+      </div>
+    )
+  }
+  //   return <Editor editorState={editorState} onChange={setEditorState} />
+  // }
+  
+  // ReactDOM.render(<WritingApp />, document.getElementById('container'))
 
 
 // function WritingApp {
@@ -101,4 +200,4 @@
 //   }
 // }
 
-//  export default WritingApp
+export default WritingApp
